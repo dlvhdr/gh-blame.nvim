334f4b9f0d9a457d76cae3ba94e06ebd9f1e6dea
Dolev Hadar <dolevc2@gmail.com>
2023-11-25 22:21:24 +0200

    first commit

--- lua/gh_blame.lua
+++ lua/gh_blame.lua
@@ -1,5 +1,5 @@
 -- main module file
-local module = require("plugin_name.module")
+local module = require("gh_blame.module")
 
 ---@class Config
 ---@field opt string Your config option
--- tests/gh_blame/gh_blame_spec.lua
+++ tests/gh_blame/gh_blame_spec.lua
@@ -1,4 +1,4 @@
-local plugin = require("plugin_name")
+local plugin = require("gh_blame")
 
 describe("setup", function()
   it("works with default", function()
