88ae4ae94a325ab499fac4aec1f5dbb97eb868c2
Dolev Hadar <dolevc2@gmail.com>
2023-12-16 16:23:06 +0200

    fix: remove test

--- tests/gh_blame/gh_blame_spec.lua
+++ tests/gh_blame/gh_blame_spec.lua
@@ -1,12 +1,3 @@
 local plugin = require("gh-blame")
 
-describe("setup", function()
-  it("works with default", function()
-    assert("my first function with param = Hello!", plugin.hello())
-  end)
-
-  it("works with custom var", function()
-    plugin.setup({ opt = "custom" })
-    assert("my first function with param = custom", plugin.hello())
-  end)
-end)
+describe("setup", function() end)
